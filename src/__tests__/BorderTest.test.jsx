1db630b9f284c0ede17fc117f5f04de2fa0798cd
FrancescoVenanti <francesco.venanti@gmail.com>
2024-01-11 17:13:23 +0100

    hgh

--- src/__tests__/BorderTest.test.jsx
+++ src/__tests__/BorderTest.test.jsx
@@ -4,9 +4,10 @@ import App from "../App";
 describe("onclick effect on books", () => {
 	it("does the book border gets red after one click", () => {
 		render(<App />);
-		const bookList = screen.queryAllByTestId("book-item");
+		const bookList = screen.getAllByTestId("book-item");
 		const firstBook = bookList[0];
 		fireEvent.click(firstBook);
+		console.log(firstBook);
 
 		expect(firstBook).toHaveStyle("border: 3px solid red");
 	});
